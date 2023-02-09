7adef14c7d12db8b4c1e50b5846ed402561c6df5
Caroline Cerussi <caroceru@gmail.com>
2023-02-09 15:14:26 -0800

    create webpack file and start setting up api key

--- src/index.js
+++ src/index.js
@@ -11,22 +11,36 @@ function getWeather (city) {
 
   //Next, we save the URL for our API call in a variable called url//
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+const url = `http://api.openweathermap.org/data/2.5/weather?p=${city}&appid=${process.env.API_KEY}`;
+
 
   //Next, we set up an event listener that listens for when our API call is complete:// 
   
-  request.addEventListener ('loadend', function () {
-    const response = JSON.parse (this.responseText);
-    if (this.status === 200) {
-      printElements (response, city);
-    }
-  });
+request.addEventListener ('loadend', function () {
+  const response = JSON.parse (this.responseText);
+  console.log (response);
+  if (this.status === 200) {
+    printElements (response, city);
+  } else {
+    // there's a new argument
+    printError (this, response, city);
+  }
+});
+
+
 
   request.open ('GET', url, true);
   request.send ();
 }
 
 // UI Logic
+
+function printError (request, city) {
+  document.querySelector (
+    '#showResponse'
+  ).innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}`;
+}
+
 
 function printElements (apiResponse, city) {
   document.querySelector (
